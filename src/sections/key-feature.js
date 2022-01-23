17ae7dbaac728025475d546488bf1124166f2b25
Bartosz Soltysik <b.soltysik90@gmail.com>
2022-01-23 19:13:46 +0100

    key feature

--- src/sections/key-feature.js
+++ src/sections/key-feature.js
@@ -14,38 +14,49 @@ const data = [
     imgSrc: Performance,
     altText: 'Fast Performance',
     title: 'Fast Performance',
-    text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
+    text: 'Get your blood tests delivered at home collect a sample from the your blood tests.'
   },
   {
     id: 2,
     imgSrc: Partnership,
     altText: 'Partnership deal',
     title: 'Partnership deal',
-    text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
+    text: 'Get your blood tests delivered at home collect a sample from the your blood tests.'
   },
   {
     id: 3,
     imgSrc: Subscription,
     altText: 'Pro Subscription',
     title: 'Pro Subscription',
-    text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
+    text: 'Get your blood tests delivered at home collect a sample from the your blood tests.'
   },
   {
     id: 4,
     imgSrc: Support,
     altText: 'Customer Support',
     title: 'Customer Support',
-    text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
-  },
+    text: 'Get your blood tests delivered at home collect a sample from the your blood tests.'
+  }
 ];
 
 export default function KeyFeature() {
   return (
-   <h1>Key Feature</h1>
+    <section sx={{ variant: 'section.keyFeature' }} id="feature">
+      <Container>
+        <SectionHeader slogan="What's the function" title="Meet the feature of our product" />
+        <Grid sx={styles.grid}>
+          {data.map((item) => (
+            <FeatureCardColumn
+              key={item.id}
+              src={item.imgSrc}
+              alt={item.altText}
+              title={item.title}
+              text={item.text}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </section>
   );
 }
 
@@ -53,21 +64,7 @@ const styles = {
   grid: {
     width: ['100%', '80%', '100%'],
     mx: 'auto',
-    gridGap: [
-      '35px 0',
-      null,
-      '40px 40px',
-      '50px 60px',
-      '30px',
-      '50px 40px',
-      '55px 90px',
-    ],
-    gridTemplateColumns: [
-      'repeat(1,1fr)',
-      null,
-      'repeat(2,1fr)',
-      null,
-      'repeat(4,1fr)',
-    ],
-  },
+    gridGap: ['35px 0', null, '40px 40px', '50px 60px', '30px', '50px 40px', '55px 90px'],
+    gridTemplateColumns: ['repeat(1,1fr)', null, 'repeat(2,1fr)', null, 'repeat(4,1fr)']
+  }
 };
