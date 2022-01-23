f0b4dca154c354f43a3999de684ed243d7c96dc4
Bartosz Soltysik <b.soltysik90@gmail.com>
2022-01-23 19:10:45 +0100

    card grid component

--- src/components/feature-card-column.js
+++ src/components/feature-card-column.js
@@ -1,14 +1,15 @@
 /** @jsx jsx */
 import { jsx, Image, Box, Heading, Text } from 'theme-ui';
 
-export default function FeatureCardColumn({
-  src,
-  altText = 'default alt text',
-  title,
-  text,
-}) {
+export default function FeatureCardColumn({ src, altText = 'default alt text', title, text }) {
   return (
-    <h1>FeatureCardColumn</h1>
+    <Box sx={styles.card}>
+      <Image src={src} altText={altText} sx={styles.img} />
+      <Box sx={styles.wrapper}>
+        <Heading sx={styles.wrapper.title}>{title}</Heading>
+        <Text sx={styles.wrapper.subTitle}>{text}</Text>
+      </Box>
+    </Box>
   );
 }
 
@@ -19,13 +20,13 @@ const styles = {
     flexDirection: 'column',
     mb: -1,
     textAlign: ['center', null, 'left'],
-    px: [4, null, 0],
+    px: [4, null, 0]
   },
   img: {
     mx: ['auto', null, 0],
     ml: ['auto', null, '-13px'],
     mb: -2,
-    width: ['80px', null, null, '90px', null, 'auto'],
+    width: ['80px', null, null, '90px', null, 'auto']
   },
   wrapper: {
     width: '100%',
@@ -36,12 +37,12 @@ const styles = {
       color: 'heading_secondary',
       lineHeight: 1.4,
       fontWeight: 700,
-      mb: [2, null, 3],
+      mb: [2, null, 3]
     },
     subTitle: {
       fontSize: 1,
       fontWeight: 400,
-      lineHeight: '1.9',
-    },
-  },
+      lineHeight: '1.9'
+    }
+  }
 };
