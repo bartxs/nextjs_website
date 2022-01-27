a29a908fb61286f8d8b6c7bfc88c7c140748bff8
Bartosz Soltysik <b.soltysik90@gmail.com>
2022-01-27 23:06:20 +0100

    feature grid

--- src/components/feature-card.js
+++ src/components/feature-card.js
@@ -1,15 +1,15 @@
 /** @jsx jsx */
 import { jsx, Image, Box, Heading, Text } from 'theme-ui';
 
-export default function FeatureCard({
-  src,
-  altText = 'default alt text',
-  title,
-  text,
-}) {
+export default function FeatureCard({ src, altText = 'default alt text', title, text }) {
   return (
-    <h1>FeatureCard</h1>
-
+    <Box sx={styles.card}>
+      <Image src={src} altText={altText} sx={styles.img} />
+      <Box sx={styles.wrapper}>
+        <Heading sx={styles.wrapper.title}>{title}</Heading>
+        <Text sx={styles.wrapper.subTitle}>{text}</Text>
+      </Box>
+    </Box>
   );
 }
 
@@ -17,7 +17,7 @@ const styles = {
   card: {
     display: 'flex',
     alignItems: 'flex-start',
-    mb: -1,
+    mb: -1
   },
 
   img: {
@@ -25,7 +25,7 @@ const styles = {
     height: 'auto',
     flexShrink: 0,
     mr: [2, 3, null, null, 4, 5],
-    ml: -2,
+    ml: -2
   },
   wrapper: {
     width: '100%',
@@ -37,13 +37,13 @@ const styles = {
       color: 'heading_secondary',
       lineHeight: 1.4,
       fontWeight: 700,
-      mb: ['10px', null, '15px'],
+      mb: ['10px', null, '15px']
     },
 
     subTitle: {
       fontSize: [1, '15px'],
       fontWeight: 400,
-      lineHeight: '1.9',
-    },
-  },
+      lineHeight: '1.9'
+    }
+  }
 };
--- src/sections/feature.js
+++ src/sections/feature.js
@@ -14,38 +14,49 @@ const data = [
     imgSrc: Performance,
     altText: 'Fast Performance',
     title: 'Fast Performance',
-    text:
-      'Let’s just get this out of the way - there will always be a kit version of Edu flow. Paid subscriptions allow us to continue helping learners around the world.',
+    text: 'Let’s just get this out of the way - there will always be a kit version of Edu flow. Paid subscriptions allow us to continue helping learners around the world.'
   },
   {
     id: 2,
     imgSrc: Partnership,
     altText: 'Pro Subscription',
     title: 'Pro Subscription',
-    text:
-      'We believe it’s important for everyone to have access to software – especially when it comes to digital learning be navigated by keyboard and screen readers.',
+    text: 'We believe it’s important for everyone to have access to software – especially when it comes to digital learning be navigated by keyboard and screen readers.'
   },
   {
     id: 3,
     imgSrc: Subscription,
     altText: 'Partnership deal',
     title: 'Partnership deal',
-    text:
-      'Let’s just get this out of the way - there will always be a kit version of Edu flow. Paid subscriptions allow us to continue helping learners around the world.',
+    text: 'Let’s just get this out of the way - there will always be a kit version of Edu flow. Paid subscriptions allow us to continue helping learners around the world.'
   },
   {
     id: 4,
     imgSrc: Support,
     altText: 'Customer Support',
     title: 'Customer Support',
-    text:
-      'We believe it’s important for everyone to have access to software – especially when it comes to digital learning be navigated by keyboard and screen readers.',
-  },
+    text: 'We believe it’s important for everyone to have access to software – especially when it comes to digital learning be navigated by keyboard and screen readers.'
+  }
 ];
 
 export default function Feature() {
   return (
-   <h1>Feature</h1>
+    <section sx={{ variant: 'section.feature' }}>
+      <Container>
+        <SectionHeader slogan="Quality Features" title="Amazing useful features" />
+        <Grid sx={styles.grid}>
+          {data.map((item) => (
+            <FeatureCard
+              key={item.id}
+              src={item.imgSrc}
+              alt={item.altText}
+              title={item.title}
+              text={item.text}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </section>
   );
 }
 
@@ -53,16 +64,7 @@ const styles = {
   grid: {
     pt: [0, null, null, null, null, null, 2],
     px: [5, 6, 0, null, 7, 8, 7],
-    gridGap: [
-      '40px 0',
-      null,
-      '45px 30px',
-      null,
-      '60px 50px',
-      '70px 50px',
-      null,
-      '80px 90px',
-    ],
-    gridTemplateColumns: ['repeat(1,1fr)', null, 'repeat(2,1fr)'],
-  },
+    gridGap: ['40px 0', null, '45px 30px', null, '60px 50px', '70px 50px', null, '80px 90px'],
+    gridTemplateColumns: ['repeat(1,1fr)', null, 'repeat(2,1fr)']
+  }
 };
